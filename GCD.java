2d593a1ef0d0f460088f41efa065f04f84aecde9
Arnav-Tiwari <arnavtiwari100@gmail.com>
2022-03-12 19:40:03 +0530

    Initial Commit

---
+++ GCD.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class Solution
+{
+	public static void main(String args[])throws IOException
+	{
+            System.out.println(gcd(3434,3434));
+        }
+public static int gcd(int a,int b)
+	{
+		if(b==0)
+		return a;
+		return gcd(b,a%b);
+	}
+}
